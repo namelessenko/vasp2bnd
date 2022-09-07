c75fa064f7321f8a911020b86632d30705892345
yapyanchzhanchzhen <artyom412254@gmail.com>
2022-09-07 12:50:44 +0200

    added calc of sibdivision

--- vasprun.py
+++ vasprun.py
@@ -1,13 +1,14 @@
-from cmath import sqrt
-from functools import reduce
-from pickle import TRUE
-from re import S
-from tkinter import N
+from array import array
+from numba import njit
+from numba import jit
+
+
+from itertools import islice
 import xml.etree.ElementTree as ET
 import func
-
+import time
 import numpy as np
-
+from math import ceil
 
 
 HARTREE = 0.036749308136649
@@ -18,47 +19,46 @@ class Vasprun:
 
     def __init__(self, vaspfile):
         self.root_node = ET.parse(vaspfile).getroot()
-        
-        self.__basis = []
-        self.__recbasis = []
+        self._basis = []
+        self._recbasis = []
         self.__eigenvalues = []
         self.__kpoints = []
 
     @property
     def a(self):
-        return self.__a
+        return self._a
 
     @property
     def b(self):
-        return self.__b
+        return self._b
 
     @property
     def c(self):
-        return self.__c
+        return self._c
 
     @property
     def b1(self):
-        return self.__b1
+        return self._b1
 
     @property
     def b2(self):
-        return self.__b2
+        return self._b2
 
     @property
     def b3(self):
-        return self.__b3
+        return self._b3
 
     @property
     def alpha(self):
-        return self.__alpha
+        return self._alpha
 
     @property
     def beta(self):
-        return self.__beta
+        return self._beta
 
     @property
     def gamma(self):
-        return self.__gamma
+        return self._gamma
 
 
     def get_structure(self):
@@ -72,27 +72,26 @@ class Vasprun:
                 for i in value:
                     if i.get('name') == 'basis':
                         for j in i:
-                            self.__basis.append(list(map(float ,j.text.split())))
+                            self._basis.append(list(map(float ,j.text.split())))
                     elif i.get('name') == 'rec_basis':
                         for j in i:
-                            self.__recbasis.append(list(map(float ,j.text.split())))  
+                            self._recbasis.append(list(map(float ,j.text.split())))  
 
         # BASIS, LATTIECE CONSTANT 
-        self.__basis = np.asanyarray(self.__basis)
-        self.__recbasis = np.asanyarray(self.__recbasis)    
-
+        self._basis = np.asanyarray(self._basis)
+        self._recbasis = np.asanyarray(self._recbasis)    
         #LAT CONST
-        self.__a=np.sqrt(np.sum(self.__basis[0,:]**2))
-        self.__b=np.sqrt(np.sum(self.__basis[1,:]**2))
-        self.__c=np.sqrt(np.sum(self.__basis[2,:]**2))
+        self._a=np.sqrt(np.sum(self._basis[0,:]**2))
+        self._b=np.sqrt(np.sum(self._basis[1,:]**2))
+        self._c=np.sqrt(np.sum(self._basis[2,:]**2))
         #REC LAT CONST
-        self.__b1 = np.sqrt(np.sum(self.recbasis[0,:]**2))
-        self.__b2 = np.sqrt(np.sum(self.recbasis[1,:]**2))
-        self.__b3 = np.sqrt(np.sum(self.recbasis[2,:]**2))
+        self._b1 = np.sqrt(np.sum(self.recbasis[0,:]**2))
+        self._b2 = np.sqrt(np.sum(self.recbasis[1,:]**2))
+        self._b3 = np.sqrt(np.sum(self.recbasis[2,:]**2))
         #Angl of lattiece
-        self.__alpha = np.arccos(np.dot(self.__basis[1,:],self.__basis[2,:])/self.__b/self.__c)*360/2/np.pi
-        self.__beta  = np.arccos(np.dot(self.__basis[0,:],self.__basis[2,:])/self.__a/self.__c)*360/2/np.pi
-        self.__gamma = np.arccos(np.dot(self.__basis[0,:],self.__basis[1,:])/self.__a/self.__b)*360/2/np.pi
+        self._alpha = np.arccos(np.dot(self._basis[1,:],self._basis[2,:])/self._b/self._c)*360/2/np.pi
+        self._beta  = np.arccos(np.dot(self._basis[0,:],self._basis[2,:])/self._a/self._c)*360/2/np.pi
+        self._gamma = np.arccos(np.dot(self._basis[0,:],self._basis[1,:])/self._a/self._b)*360/2/np.pi
 
 
     @property
@@ -101,12 +100,11 @@ class Vasprun:
     
     @property
     def basis(self):
-        return self.__basis
+        return self._basis
 
     @property
     def recbasis(self):
-        return self.__recbasis
-
+        return self._recbasis
 
     def get_eigenvalues(self):
         """
@@ -182,20 +180,43 @@ class Vasprun:
                 self.subdivision.append(list(map(float, tag.text.split())))
             elif key == 'genvec3':
                 self.subdivision.append(list(map(float, tag.text.split())))
-        print(self.subdivision)
 
         self.subdivision = np.asanyarray(self.subdivision)
         self.h1=np.sqrt(np.sum(self.subdivision[0,:]**2))
         self.h2=np.sqrt(np.sum(self.subdivision[1,:]**2))
         self.h3=np.sqrt(np.sum(self.subdivision[2,:]**2))
 
-        self.N1 = int((self.b1*3)/self.h1)
-        self.N2 = int((self.b2*3)/self.h2)
-        self.N3 = int((self.b3*3)/self.h3)
+        self.N1 = int(ceil((self.b1*3)/self.h1))
+        self.N2 = int(ceil((self.b2*3*(self.b1/self.b2))/self.h2))
+        self.N3 = int(ceil((self.b3*3*(self.b1/self.b3))/self.h3))
+       
+       
+        # tmp1 = 0
+        # tmp2 = 0
+        # tmp3 = 0
+        # for tag in self.root_node.findall('kpoints/varray'):
+        #     value = tag.get('name')
+        #     if value == 'kpointlist':
+        #         for item in tag.findall('v'):
+        #             self.__kpoints.append(item.text.split())
+
+        # for i in range(len(self.__kpoints)):
+        #     if self.__kpoints[i][0] > 0:
+        #         tmp1 = self.__kpoints[i][0]
+        #         break
+        # for i in range(len(self.__kpoints)):
+        #     if self.__kpoints[i][1] > 0:
+        #         tmp2 = self.__kpoints[i][1]
+        #         break
+        # for i in range(len(self.__kpoints)):
+        #     if self.__kpoints[i][2] > 0:
+        #         tmp3 = self.__kpoints[i][2]
+        #         break
                 
-
-
-        
+        # print(tmp1)
+        # print(tmp2)
+        # print(tmp3)
+                    
 class Vasp2Igor(Vasprun):
     """
     Conevert Vasp out to Igor input
@@ -208,78 +229,90 @@ class Vasp2Igor(Vasprun):
         super().get_kpoints()
         super().get_electronicparam()
         super().get_subdivision()
+        self.reducebasis()
+        self.hkpoints()
         
-
-
+    
+    
     def reducebasis(self):
-        self.__reduced_a = self.__a
-        self.__reduced_b = self.__b / self.__a
-        self.__reduced_c = self.__c / self.__a
-
-        self.__reduced_lattiece_constant = np.array([self.__reduced_a/BOHR, self.__reduced_b, self.__reduced_c])
-        self.__reduced_basis = np.divide(self.__basis, (self.__reduced_a))
-        self.__reduced_rec_basis = np.multiply(self.__rec_basis, (2 * np.pi * self.__reduced_a))
+        self._reduced_a = self._a
+        self._reduced_b = self._b / self._a
+        self._reduced_c = self._c / self._a
 
 
+        self.__reduced_lattiece_constant = self._reduced_a/BOHR, self._reduced_b, self._reduced_c #reduced constant *BOHR
+        self.__reduced_basis = np.divide(self._basis, (self._reduced_a))
+        self.__reduced_rec_basis = np.multiply(self._recbasis, (2 * np.pi * self._reduced_a))
+
+    
     def bandstransform(self):
         """Reading and replace eigevalues to eig*=HARTEE"""
 
         transformedbands = []
 
         for i in super().eigenvalues:
-            transformedbands.append(i*HARTREE)
+            transformedbands.append(i*HARTREE)#and round just for macros
 
         transformedbands = list(func.list_split(transformedbands, int(self.get_electronicparam()["NBANDS"])))
 
         return transformedbands
 
-
+    
     def transformkpoints(self):
 
         transformedkpoints = []
 
         for kpoints in super().kpoints:
             for kpoint in kpoints:
-                transformedkpoints.append(kpoint*(2/1.73250))
+                transformedkpoints.append(kpoint*(2/1.73250))#and round just for macros
         transformedkpoints = list(func.list_split(transformedkpoints, 3))
-
         return transformedkpoints
+        
+    def hkpoints(self):
+        with open('ignore/KPOINTS') as f:
+            self._txt = f.readlines()[4:]
+            
 
-    
     def writefile(self, name):
         with open(name, 'w') as f:
-            f.write(str(self.efermi))
-            f.write()
+            for i in self.__reduced_lattiece_constant:
+                f.write(str(i)+"\t")
+            f.write("\n\n")
+            
+            for i in self._txt:
+                f.write(i)
+            f.write("\n\n")
+
+            f.write(str(self.efermi*HARTREE))
+            f.write("\n\n")
+
+            for i in self.__reduced_basis:
+                for j in i:
+                    f.write(str(j)+"\t")
+                f.write("\n")
+            f.write("\n")
+
+            for i in self.__reduced_rec_basis:
+                for j in i:
+                    f.write(str(j)+"\t")
+                f.write("\n")
+            f.write("\n")
+
+
             for i, (kpoint, band) in enumerate(zip(self.transformkpoints(), self.bandstransform()),start=1):
                 # f.writelines(str(kpoint)[1:-1]+ '        /' + str(i) + '\n')
                 # f.writelines(str(band)[1:-1]+'\n')
                 f.write('  '.join(map(str, kpoint))+ '        /' + str(i) + '\n')
                 f.write('  '.join(map(str, band))+'\n')
-
+           
 
 
 if __name__ == "__main__":
-    # v = Vasp2Igor('vasprunrandom.xml')
-    # v.get_kpoints()
-    # v.get_eigenvalues()
-    # v.get_structure()
-    # v.writefile('tets2')
-    v = Vasp2Igor('ignore/vasprun14127.xml')
-    
+    start_time = time.time()
+    v = Vasp2Igor('ignore/vasprunISYM.xml') 
     print(v.N1)
     print(v.N2)
-    print(v.N3)
-    # print(v.alpha, '\n', v.beta, '\n', v.gamma, '\n', v.a, '\n', v.b, '\n', v.c, '\n', v.b1, v.b2, v.b3)
-    
-    # N1 =14
-    # N2 =12
-    # N3 =7
-
-    # print(v.b1/N1*3)
-    # print(v.b2/N2*3)
-    # print(v.b3/N3*3)
-    
-    
-    # print(int((v.b1*3)/0.0726))
-    # print(int((v.b2*3)/0.08333333))
-    
+    print(v.N3) 
+    v.writefile("test.BND")  
+    print("--- %s seconds ---" % (time.time() - start_time))
+ 
